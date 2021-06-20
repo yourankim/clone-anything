48f21e439053a00d0d871cb361768409bf5ef625
yourankim <youran.dev@gmail.com>
2021-06-20 22:47:24 +0900

    Update PerformanceDetail Component

--- virtualstation2021/performance.js
+++ virtualstation2021/performance.js
@@ -1,13 +1,31 @@
+import PerformanceDetail from './performance-detail.js';
+
 const Performance = (data, $selector) => {
-  console.log(data);
   const list = data.list
-    .map((d) => {
-      return `<dt>${d.artist}</dt><dd>${d.title}</dd><dd>${d.type}</dd>`;
+    .map((d, index) => {
+      return `<button class="show-detail" id=${index}><dt>${d.artist}</dt><dd>${d.title}</dd></a><dd>${d.type}</dd></button>`;
     })
     .join('');
 
   $selector.innerHTML = `<h3>${data.title}</h3><dl>${list}</dl>`;
-  console.log(list);
+
+  const closeDetailPage = () => {
+    alert('close!');
+  };
+
+  const details = document.querySelectorAll('.show-detail');
+  details.forEach((element) => {
+    element.addEventListener('click', (e) => {
+      const detailPage = PerformanceDetail(
+        data.list[element.id],
+        element.id,
+        closeDetailPage
+      );
+
+      element.parentElement.appendChild(detailPage);
+      element.hidden = true;
+    });
+  });
 };
 
 export default Performance;
